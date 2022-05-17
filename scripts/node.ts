8aa6a0f91013660b5901c6b4be314307546584f8
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-17 18:18:39 -0500

    :recycle: Clean up
    Remember Summer Days

---
+++ scripts/node.ts
@@ -0,0 +1,11 @@
+import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
+
+buildPackage({
+  repoName: "timezones",
+  name: "@ultirequiem/fibonacci",
+  description: "🔢 Fibonacci Succession Utilities + CLI Tool",
+  homepage: "https://ulti.js.org/fibonacci.js",
+  keywords: ["fibonacci", "math"],
+  license: "MIT",
+  version: "1.3.0",
+});
--- test.ts
+++ test.ts
@@ -4,7 +4,6 @@ import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
 
 Deno.test("fibonacci", () => {
   const result = fibonacci(50);
-  console.log(result);
 
   assertEquals(result, 12586269025);
 });
