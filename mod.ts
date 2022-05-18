2252d909a5bd3ca3c6bfc62b67358b2e5c685934
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-18 18:27:47 -0500

    :recycle: Update

--- mod.ts
+++ mod.ts
@@ -3,63 +3,24 @@ export function fibonacci(top: number) {
 
   let [starter, second] = [0, 1];
 
-  for (let index = 1; index < top; index++) {
+  for (let index = 0; index < top; index++) {
     [starter, second] = [second, starter + second];
   }
 
   return starter;
 }
 
-export function listFibonacci(top: number) {
-  biggerThanZero(top);
-
-  const fibonacci = [0, 1];
-
-  for (let index = 1; index < top; index++) {
-    fibonacci.push(fibonacci[index] + fibonacci[index - 1]);
-  }
-
-  return fibonacci;
-}
-
-export function* lazyFibonacc(n: number) {
-  biggerThanZero(n);
-
-  let first = 0;
-  let second = 1;
-
-  yield first;
-
-  yield second;
-
-  const top = n - 2;
-
-  let current = 1;
-
-  let count = 0;
-
-  while (count <= top) {
-    current = second;
-
-    yield current;
-
-    second = first + second;
-    first = current;
-
-    count++;
-  }
-}
-
 /**
- * @private
  * Throws if the number is lower than zero.
+ *
+ * @private
  */
 function biggerThanZero(number: number) {
   if (typeof number !== "number") {
-    throw new TypeError(`Exepected a number, but got ${typeof number}.`);
+    throw new TypeError("Exepected a number.");
   }
 
-  if (number === 0 || number < 0) {
-    throw new Error(`Expected a number bigger than zero, but got ${number}.`);
+  if (number < 0) {
+    throw new RangeError("Expected a number bigger than zero.");
   }
 }
--- test.ts
+++ test.ts
@@ -1,12 +1,29 @@
 import { fibonacci } from "./mod.ts";
 
-import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.137.0/testing/asserts.ts";
 
 Deno.test("fibonacci", () => {
   // Taken from https://wikipedia.org/wiki/Fibonacci_number
   const fibonacciSequenceStart = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
 
   for (let index = 0; index < fibonacciSequenceStart.length; index++) {
-    assertEquals(fibonacci(index + 1), fibonacciSequenceStart[index]);
+    assertEquals(fibonacci(index), fibonacciSequenceStart[index]);
   }
+
+  assertThrows(() => {
+    // @ts-ignore: It should throw
+    fibonacci("hello");
+
+    // @ts-ignore: It should throw
+    fibonacci(new Set([1, 2, 3, 4, 5]));
+  }, TypeError);
+
+  assertThrows(() => {
+    fibonacci(-3);
+
+    fibonacci(-25);
+  }, RangeError);
 });
